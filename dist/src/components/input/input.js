16dcfd5cda60dfff69d4dd44d68c2d54a8fab737
Markus Hederström <markus.hederstrom@humany.com>
2018-09-01 11:12:39 +0200

    0.1.2

--- dist/src/components/input/input.js
+++ dist/src/components/input/input.js
@@ -20,6 +20,15 @@ var __assign = (this && this.__assign) || Object.assign || function(t) {
     }
     return t;
 };
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) if (e.indexOf(p[i]) < 0)
+            t[p[i]] = s[p[i]];
+    return t;
+};
 import * as React from 'react';
 import styled from '../../styled/styled-components';
 var Input = /** @class */ (function (_super) {
@@ -39,11 +48,16 @@ var Input = /** @class */ (function (_super) {
         return _this;
     }
     Input.prototype.render = function () {
-        return React.createElement(StyledInput, __assign({}, this.props, { onChange: this.handleOnChange }));
+        var _a = this.props, multiline = _a.multiline, rest = __rest(_a, ["multiline"]);
+        if (multiline) {
+            return React.createElement(StyledTextarea, __assign({}, rest, { onChange: this.handleOnChange }));
+        }
+        return React.createElement(StyledInput, __assign({}, rest, { onChange: this.handleOnChange }));
     };
     return Input;
 }(React.Component));
 export var StyledInput = styled.input(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  margin: 0;\n  max-width: 100%;\n  outline: 0;\n  text-align: left;\n  line-height: 1.2em;\n  padding: 0.67em 1em;\n  background: #fff;\n  border: 1px solid rgba(34, 36, 38, 0.15);\n  color: rgba(0, 0, 0, 0.87);\n  border-radius: 0.28571429rem;\n  -webkit-transition: border-color 0.1s ease, -webkit-box-shadow 0.1s ease;\n  transition: border-color 0.1s ease, -webkit-box-shadow 0.1s ease;\n  transition: box-shadow 0.1s ease, border-color 0.1s ease;\n  transition: box-shadow 0.1s ease, border-color 0.1s ease,\n    -webkit-box-shadow 0.1s ease;\n  -webkit-box-shadow: none;\n  box-shadow: none;\n  font-family: inherit;\n\n  box-sizing: border-box;\n\n  &:focus {\n    border-color: #85b7d9;\n    background: #fff;\n    color: rgba(0, 0, 0, 0.8);\n    -webkit-box-shadow: none;\n    box-shadow: none;\n  }\n"], ["\n  margin: 0;\n  max-width: 100%;\n  outline: 0;\n  text-align: left;\n  line-height: 1.2em;\n  padding: 0.67em 1em;\n  background: #fff;\n  border: 1px solid rgba(34, 36, 38, 0.15);\n  color: rgba(0, 0, 0, 0.87);\n  border-radius: 0.28571429rem;\n  -webkit-transition: border-color 0.1s ease, -webkit-box-shadow 0.1s ease;\n  transition: border-color 0.1s ease, -webkit-box-shadow 0.1s ease;\n  transition: box-shadow 0.1s ease, border-color 0.1s ease;\n  transition: box-shadow 0.1s ease, border-color 0.1s ease,\n    -webkit-box-shadow 0.1s ease;\n  -webkit-box-shadow: none;\n  box-shadow: none;\n  font-family: inherit;\n\n  box-sizing: border-box;\n\n  &:focus {\n    border-color: #85b7d9;\n    background: #fff;\n    color: rgba(0, 0, 0, 0.8);\n    -webkit-box-shadow: none;\n    box-shadow: none;\n  }\n"])));
+export var StyledTextarea = styled.textarea(templateObject_2 || (templateObject_2 = __makeTemplateObject(["\n  margin: 0;\n  max-width: 100%;\n  outline: 0;\n  text-align: left;\n  line-height: 1.2em;\n  padding: 0.67em 1em;\n  background: #fff;\n  border: 1px solid rgba(34, 36, 38, 0.15);\n  color: rgba(0, 0, 0, 0.87);\n  border-radius: 0.28571429rem;\n  -webkit-transition: border-color 0.1s ease, -webkit-box-shadow 0.1s ease;\n  transition: border-color 0.1s ease, -webkit-box-shadow 0.1s ease;\n  transition: box-shadow 0.1s ease, border-color 0.1s ease;\n  transition: box-shadow 0.1s ease, border-color 0.1s ease,\n    -webkit-box-shadow 0.1s ease;\n  -webkit-box-shadow: none;\n  box-shadow: none;\n  font-family: inherit;\n\n  box-sizing: border-box;\n\n  &:focus {\n    border-color: #85b7d9;\n    background: #fff;\n    color: rgba(0, 0, 0, 0.8);\n    -webkit-box-shadow: none;\n    box-shadow: none;\n  }\n"], ["\n  margin: 0;\n  max-width: 100%;\n  outline: 0;\n  text-align: left;\n  line-height: 1.2em;\n  padding: 0.67em 1em;\n  background: #fff;\n  border: 1px solid rgba(34, 36, 38, 0.15);\n  color: rgba(0, 0, 0, 0.87);\n  border-radius: 0.28571429rem;\n  -webkit-transition: border-color 0.1s ease, -webkit-box-shadow 0.1s ease;\n  transition: border-color 0.1s ease, -webkit-box-shadow 0.1s ease;\n  transition: box-shadow 0.1s ease, border-color 0.1s ease;\n  transition: box-shadow 0.1s ease, border-color 0.1s ease,\n    -webkit-box-shadow 0.1s ease;\n  -webkit-box-shadow: none;\n  box-shadow: none;\n  font-family: inherit;\n\n  box-sizing: border-box;\n\n  &:focus {\n    border-color: #85b7d9;\n    background: #fff;\n    color: rgba(0, 0, 0, 0.8);\n    -webkit-box-shadow: none;\n    box-shadow: none;\n  }\n"])));
 export default Input;
-var templateObject_1;
+var templateObject_1, templateObject_2;
 //# sourceMappingURL=input.js.map
--- dist/src/components/input/input.stories.js
+++ dist/src/components/input/input.stories.js
@@ -2,5 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Input from './input';
 import { wInfo } from '../../../utils';
-storiesOf('Components/Input', module).addWithJSX('basic Input', wInfo("\n\n  ### Notes\n\n  This is a standard <Input />\n\n  ### Usage\n  ~~~js\n  <Input />\n  ~~~")(function () { return React.createElement(Input, { placeholder: "hello" }); }));
+storiesOf('Components/Input', module)
+    .addWithJSX('basic Input', wInfo("\n\n  ### Notes\n\n  This is a standard <Input />\n\n  ### Usage\n  ~~~js\n  <Input />\n  ~~~")(function () { return React.createElement(Input, { placeholder: "hello" }); }))
+    .addWithJSX('multiline Input', wInfo("\n\n  ### Notes\n\n  This is a multiline <Input />\n\n  ### Usage\n  ~~~js\n  <Input multiline/>\n  ~~~")(function () { return React.createElement(Input, { placeholder: "hello", multiline: true }); }));
 //# sourceMappingURL=input.stories.js.map
