d3a3c8c31c555013fd9af115e83fcb1fddc93dd9
Markus Hederström <markus.hederstrom@humany.com>
2018-09-03 17:41:43 +0200

    added more colors to label

--- src/components/label/label.tsx
+++ src/components/label/label.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
-import styled from 'styled-components';
+
+import styled, { css } from '../../styled/styled-components';
 
 export interface LabelProps {
   children: any;
@@ -10,14 +11,54 @@ export interface LabelProps {
 const StyledLabel = styled.span`
   font-family: 'Roboto', sans-serif;
   display: inline-block;
-  padding: 2px 6px;
-  border-radius: 20px;
+  padding: 2px 8px;
+  border-radius: 4px;
   color: #fff;
-  font-size: 0.8rem;
-  background: #00b5ad;
+  font-size: 0.8em;
+
   margin: 0 3px;
 
-  ${props => props.color === 'orange' && 'background: #cf590c !important;'};
+  ${({ color }) => {
+    if (color === 'orange') {
+      return css`
+        background: #cf590c;
+      `;
+    }
+    if (color === 'red') {
+      return css`
+        background: #db2828;
+      `;
+    }
+    if (color === 'purple') {
+      return css`
+        background: #a333c8;
+      `;
+    }
+    if (color === 'green') {
+      return css`
+        background: #21ba45;
+      `;
+    }
+    if (color === 'yellow') {
+      return css`
+        background: #fbbd08;
+      `;
+    }
+    if (color === 'blue') {
+      return css`
+        background: #2185d0;
+      `;
+    }
+    if (color === 'teal') {
+      return css`
+        background: #00b5ad;
+      `;
+    }
+    return css`
+      background: #e8e8e8;
+      color: rgba(0, 0, 0, 0.6);
+    `;
+  }};
 `;
 
 const Label: React.StatelessComponent<LabelProps> = ({
